717cb32c09d0af3d16de429d623908eb33153930
Kujeasade <132667925+Kujeasade@users.noreply.github.com>
2023-05-30 08:35:40 +0100

    Update Physics.java

--- src/Physics.java
+++ src/Physics.java
@@ -15,3 +15,40 @@ public class Physics {
         //displaying answer
         System.out.println(kineticEnergy);
     }
+    public static void potentialEnergy() {
+        Scanner scan = new Scanner(System.in);
+
+        //mass input
+        int mass = scan.nextInt();
+        //gravity
+        int gravity = scan.nextInt();
+        //height input
+        int height = scan.nextInt();
+        //potential energy formula
+        int potentialEnergy = mass * gravity * height;
+        System.out.println(potentialEnergy);
+    }
+    public static void power() {
+        //read input
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter work");
+        int work = scan.nextInt();
+        System.out.println("Enter work");
+        int time = scan.nextInt();
+
+        double pow = work * time;
+        System.out.println("Power is " + pow + "Watts");
+    }
+
+    public static float getGravity() {
+        return Gravity;
+    }
+
+    public void setGravity(float Gravity) {
+        this.Gravity = Gravity;
+    }
+
+    private static float Gravity = 10;
+}
+
